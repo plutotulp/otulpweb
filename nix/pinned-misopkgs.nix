4f7f9b81f8404891e7c064009807d3b6d88daa64
Erlend Pedersen <erlendpe@online.no>
2021-05-05 21:34:40 +0200

    Oppdaterte misopkgs.

--- nix/pinned-misopkgs.nix
+++ nix/pinned-misopkgs.nix
@@ -2,6 +2,6 @@ import (builtins.fetchTarball {
   # A Miso version of nixpkgs. Use with cachix "cachix use miso" to
   # avoid waiting forever for GHCJS to compile (and then running out
   # of memory before it finishes).
-  url = "https://github.com/dmjio/miso/archive/561ffad.tar.gz";
-  sha256 = "1wwzckz2qxb873wdkwqmx9gmh0wshcdxi7gjwkba0q51jnkfdi41";
+  url = "https://github.com/dmjio/miso/archive/ea25964565074e73d4052b56b60b6e101fa08bc5.tar.gz";
+  sha256 = "1yb9yvc0ln4yn1jk2k5kwwa1s32310abawz40yd8cqqkm1z7w6wg";
 }) {}
