7ac1b17d04371089ad340a9b6d0c6dba89279290
Erlend Pedersen <erlendpe@online.no>
2021-06-20 01:38:37 +0200

    Litt kommentarer.

--- otulpweb-common/src/OtulpWeb/Common/Rot.hs
+++ otulpweb-common/src/OtulpWeb/Common/Rot.hs
@@ -31,15 +31,22 @@ import Test.QuickCheck (Arbitrary)
 newtype RotKey = RotKey { unRotKey :: Int }
   deriving (Arbitrary, Eq, Num, Ord, Show)
 
+-- | All the integers, in order, representing valid ROT characters,
+-- starting from 0 (for 'a').
 validCharInts :: Seq Int
 validCharInts = [0..28]
 
+-- | Predicate. Is the given integer valid as a representation for a ROT
+-- character?
 isValidCharInt :: Int -> Bool
 isValidCharInt i = isJust (Seq.elemIndexL i validCharInts)
 
+-- | All the valid ROT characters, in alphabetical order, starting
+-- from 'a'.
 validChars :: Seq Char
 validChars = ['a'..'z'] <> ['æ', 'ø', 'å']
 
+-- | Predicate. Is the given character a valid ROT character?
 isValidChar :: Char -> Bool
 isValidChar c = isJust (Seq.elemIndexL c validChars)
 
@@ -49,6 +56,10 @@ numValidChars = Seq.length validChars
 aInt :: Int
 aInt = fromEnum 'a'
 
+-- | Yield the integer representation of the given character, provided
+-- that it actually is a valid ROT character. The resulting integer is
+-- a member of the set of integers fom 'validCharInts'. If the input
+-- is not in the ROT character set, the result is 'Nothing'.
 charToInt :: Char -> Maybe Int
 charToInt (fromEnum -> i)
   | i == æInt = Just 26
@@ -79,11 +90,11 @@ rot (RotKey n) i = (n + i) `mod` numValidChars
 validText :: String -> String
 validText = filter isValidChar
 
--- ROT-encrypt lowercase Char into uppercase Char.
+-- | ROT-encrypt lowercase Char into uppercase Char.
 rotEnc :: RotKey -> Char -> Maybe Char
 rotEnc k c = toUpper <$> (charToInt c >>= intToChar . rot k)
 
--- ROT-decrypt uppercase Char into lowercase Char.
+-- | ROT-decrypt uppercase Char into lowercase Char.
 rotDec :: RotKey -> Char -> Maybe Char
 rotDec k (toLower -> c) = charToInt c >>= intToChar . rot (negate k)
 
