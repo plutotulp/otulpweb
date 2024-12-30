7b82f18ece91d1c319a8347ae392cff11aa93e9d
Erlend Pedersen <erlendpe@online.no>
2024-12-30 13:46:32 +0100

    +NixOS module for service

--- flake.nix
+++ flake.nix
@@ -19,8 +19,33 @@
         inherit pkgs gitignore;
         miso = import miso { inherit system; };
       };
+
+      otulpweb-module = { lib, config, pkgs, ... }:
+        let
+          cfg = config.services.otulpweb;
+        in
+        {
+          imports = [];
+
+          options.services.otulpweb = {
+            enable = lib.mkOption {
+              default = false;
+              example = true;
+              description = ''
+          Run otulpweb service. Default configuration listens on port 8080 and
+          uses all available cores.
+          '';
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            environment.systemPackages = [ otulpweb.otulpweb-deployment ];
+            systemd.services.otulpweb = otulpweb.otulpweb-service;
+          };
+        };
     in
     {
       packages.${system}.default = otulpweb.otulpweb-deployment;
+      nixosModules.default = otulpweb-module;
     };
 }
