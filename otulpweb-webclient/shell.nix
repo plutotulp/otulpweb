0b73ed13907d2fbe81d82c1e6db418b8829fa6e0
Erlend Pedersen <erlendpe@online.no>
2025-06-08 12:37:50 +0200

    Kaster hele gamle otulpweb.
    GHCJS er for mye styr. Jeg har lyst til å prøve gleam og lustre litt
    istedet.

--- otulpweb-webclient/shell.nix
+++
@@ -1,8 +0,0 @@
-#  # ghcid session for library code
-#  nix-shell --run run-ghcid
-#
-#  # ghcid session for test suite
-#  nix-shell --run run-ghcid test
-
-let proj = import ../. {};
-in (import ../nix/mkShell.nix) proj.devTools proj.otulpweb-webclient.ghc
